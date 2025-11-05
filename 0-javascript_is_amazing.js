ee6132b2aa58e9edaa0f238c948620e2fee32c6e
Abdulbaki <abdoladepo@gmail.com>
2025-11-05 18:19:02 +0100

    First constant, first print

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,27 @@
+const myVar = "JavaScript is amazing";
+console.log(myVar)
+
+
+
+// let number = 30;
+// if(number > 0)
+// {
+//     console.log(number, "is a positive integer.");
+// } 
+// else if(number < 0)
+// {
+//     console.log(number, "is a negative integer");
+// }
+// else 
+// {
+//     console.log(number, "is zero.");
+// }
+
+// for(i =1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// function sum(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(sum(3, 12));
