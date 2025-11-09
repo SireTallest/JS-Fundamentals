cda057a2a9d6c37450872f2402a272184ee44550
Abdulbaki <abdoladepo@gmail.com>
2025-11-09 20:54:45 +0100

    Using script and function to perform basic operatioon

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return "NaN";
+  } else {
+    return a + b;
+  }
+}
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+console.log(add(a, b));
